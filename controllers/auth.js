51890f129eb2c41f16d96e34ce8cdbf4b27af772
Alin Chiuaru <alin.chiuaru@gmail.com>
2016-04-23 01:26:58 +0300

    rename routes to controllers

---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+var User    = models.User;
+var jwt     = require('jsonwebtoken');
+var config  = require('../config');
+
+
+function generateToken(user) {
+    return jwt.sign({ user: user }, config.secret, {
+        expiresIn: 600 * 60 //sec
+    });
+}
+
+router.post('/auth', function(req, res) {
+    User.findOne({ where: { username: req.body.username } })
+        .then(function(user) {
+
+            if ( !user || user.password !== req.body.password  ) {
+                res.json({ succcess: false, message: 'Username or password is incorrect!' });
+            } else {
+
+                var token = generateToken(user);
+                res.json({ succcess: true, message: 'You have been sucessfully authenticated!', token: token });
+            }
+        });
+});
+
+
+module.exports = router;
