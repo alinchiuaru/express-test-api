623e559589cd4076240ba5ea85cc974ea9853dce
Alin Chiuaru <alin.chiuaru@gmail.com>
2016-06-29 19:22:36 +0300

    sort before comparing

--- validators/MCValidator.js
+++ validators/MCValidator.js
@@ -3,6 +3,23 @@ function validate( userInputData, questionData ) {
     var questionData = JSON.parse(questionData.replace("\r\n", "\\n")).answers,
         errors = 0;
 
+    function sortByAnswerLabel(a, b) {
+        if ( a.answer > b.answer ) {
+            return 1;
+        }
+
+        if ( a.answer < b.answer ) {
+            return -1;
+        }
+
+        if ( a.answer == b.answer ) {
+            return 0;
+        }
+    }
+
+    userInputData.sort(sortByAnswerLabel);
+    questionData.sort(sortByAnswerLabel);
+
     userInputData.forEach(function(answer, index) {
         if ( answer.correct !== questionData[index].correct ) {
             errors++;
