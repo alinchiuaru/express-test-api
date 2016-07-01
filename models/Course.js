18920ba7fd0136961d49127a28ab89e13694db57
Alin Chiuaru <alin.chiuaru@gmail.com>
2016-07-01 03:51:34 +0300

    update foreignKey references

--- models/Course.js
+++ models/Course.js
@@ -23,8 +23,8 @@ module.exports = function (sequelize, DataTypes) {
         {
             classMethods:{
                 associate:function(models) {
-                    Course.hasMany(models.Chapter, { foreignKey: 'id', as: 'chapters'} );
-                    Course.hasMany(models.Quiz, { foreignKey: 'id', as: 'quizzes'} );
+                    Course.hasMany(models.Chapter, { foreignKey: 'courseId', as: 'chapters'} );
+                    Course.hasMany(models.Quiz, { foreignKey: 'courseId', as: 'quizzes'} );
                 }
             }
         }
