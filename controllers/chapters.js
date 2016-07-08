eb3b5f31331ac3c6a2de789b4aa371b635a2c5d0
Alin Chiuaru <alin.chiuaru@gmail.com>
2016-07-09 02:08:11 +0300

    chapters

--- controllers/chapters.js
+++ controllers/chapters.js
@@ -9,9 +9,20 @@ const querystring = require('querystring');
 
 router.get('/chapters', authorization.adminAccess, function(req, res) {
     Chapter.findAll({
-        attributes : ['id', 'title', 'description', 'lectureNode']
+        attributes : ['id', 'title', 'description', 'lectureNote']
     }).then(function(data) {
         res.json(data);
+    });
+});
+
+
+router.get('/chapters/:chapterId', authorization.regularAccess, function(req, res) {
+    Chapter.findOne({
+        attributes : ['id', 'title', 'description', 'lectureNote'],
+        where: { id: req.params.chapterId }
+    }).then(function(data) {
+        var lectureNote = querystring.unescape(data.lectureNote);
+        res.json({ success: true, data: Object.assign(data, { lectureNote }) });
     });
 });
 
